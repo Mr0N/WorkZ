74355841de0d9502f85a4e0adf17540a2c52bf8e
new_user <new_user@gmail.com>
2021-03-29 22:13:09 +0300

    Добавьте файлы проекта.

---
+++ WorkZ/Program.cs
@@ -0,0 +1,17 @@
+﻿using SourceGeneratorSamples;
+using System;
+using WorkZ;
+
+
+namespace WorkZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            HelloWorldGenerator.HelloWorldGenerated.SayHello(); // calls Console.WriteLine("Hello World!") and then prints out syntax trees
+           
+        }
+    }
+}
